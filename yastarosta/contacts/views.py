407bcca0125e350600c5fc070d712ea70048cb15
dima <d.kukushkin@agora.ru>
2022-03-15 12:09:08 +0300

    loginrequiredmixin fix

--- yastarosta/contacts/views.py
+++ yastarosta/contacts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView, CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-class ContactView(TemplateView):
+
+class ContactView(LoginRequiredMixin, TemplateView):
     template_name = 'contacts.html'
 
     def get_context_data(self, **kwargs):
