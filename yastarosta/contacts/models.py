59719f45dbe139bc749882fc364adbba7697fdfb
dima <d.kukushkin@agora.ru>
2022-03-15 13:12:19 +0300

    contacts added template and first model

--- yastarosta/contacts/models.py
+++ yastarosta/contacts/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
-# class Contact(models.Model):
-#     name = models.CharField(max_length=300)
-#     type = models.CharField(max_length=100)
-#     subject = models.CharField(max_length=200)
-#     phone = models.CharField(max_length=20)
-#     email = models.CharField(max_length=100)
-#     photo = models.ImageField()
-#     location = models.CharField(max_length=2000)
-#
-#     extra_info = models.TextField()
+
+class Contact(models.Model):
+    name = models.CharField(max_length=300)
+    type = models.CharField(max_length=100)
+    subject = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    # photo = models.ImageField()
+    location = models.CharField(max_length=2000)
+    extra_info = models.TextField()
--- yastarosta/contacts/views.py
+++ yastarosta/contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView, CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from yastarosta.contacts.models import Contact
 
 
 class ContactView(LoginRequiredMixin, TemplateView):
@@ -8,5 +9,7 @@ class ContactView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        contacts = Contact.objects.all()
+        context['contacts'] = contacts
         return context
 contact_view = ContactView.as_view()
