45b551e174e92fa878d8bc502dcb725573b19aa3
dima <d.kukushkin@agora.ru>
2022-03-22 10:24:42 +0300

    student group html and view

--- yastarosta/student_group/views.py
+++ yastarosta/student_group/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, DetailView, CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class StudentGroupView(LoginRequiredMixin, TemplateView):
+    template_name = 'student_group.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+contact_view = StudentGroupView.as_view()
